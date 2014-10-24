6d585b188d16a05bfe2740f8781502000e5c2733
djluo <dj.luo@baoyugame.com>
2014-10-24 18:12:19 +0000

    运行在docker中的firefox

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# run firefox
+#
+FROM debian:latest
+#MAINTAINER djluo <dj.luo@baoyugame.com>
+
+ADD ./sources.list /etc/apt/
+# 使用iceweasel来解决依然问题
+RUN apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3EE67F3D0FF405B2 \
+    && apt-get update  \
+    && apt-get install -y fonts-wqy-microhei fonts-wqy-zenhei fonts-dejavu locales \
+    && apt-get install -y iceweasel \
+    && apt-get remove  -y iceweasel \
+    && apt-get install -y firefox firefox-l10n-zh \
+    && /usr/sbin/useradd -u 1000 -m browser
+
+# 设置中文环境
+RUN sed -e '/en_US.UTF-8/s/^# //' -e '/zh_CN.UTF-8/s/^# //' /etc/locale.gen -i \
+    && locale-gen && export LANG=zh_CN.utf8 LC_ALL=zh_CN.utf8
+
+USER browser
+ENV  HOME   /home/browser
+ENV  LANG   zh_CN.utf8
+ENV  LC_ALL zh_CN.utf8
+CMD  /usr/bin/firefox
---
+++ run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# vim:set et ts=2 sw=2:
+
+current_dir=`dirname $0`
+current_dir=`readlink -f $current_dir`
+cd ${current_dir} && export current_dir
+
+app="firefox"
+port=""
+
+is_exists() {
+  sudo docker inspect --format='{{ .State.Running }}' $app | egrep "true|false" >/dev/null
+  if [ $? -ne 0 ] ;then
+    return 1
+  fi
+  return 0
+}
+run() {
+  local mode="-d"
+
+  if [ "x$1" == "xdebug" ];then
+    local mode="-ti --rm"
+    local app="debug_$app"
+    unset port
+    shift
+  fi
+
+  local cmd="$@"
+
+  sudo docker run $mode $port           \
+       --link ssh-proxy:ssh_proxy       \
+       -e DISPLAY=$DISPLAY              \
+       -e XMODIFIERS=$XMODIFIERS        \
+       -e GTK_IM_MODULE=$GTK_IM_MODULE  \
+       -e QT_IM_MODULE=$QT_IM_MODULE    \
+       -e QT4_IM_MODULE=$QT4_IM_MODULE  \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v ${HOME}/.mozilla/:/home/browser/.mozilla/   \
+       -v ${HOME}/downloads/:/home/browser/downloads/ \
+      -e "TZ=Asia/Shanghai"        \
+      --name ${app} ${app}         \
+      /bin/sh -c "exec /usr/bin/firefox"
+}
+usage() {
+  echo "Usage: $0 [start|stop|debug|rebuild]"
+  exit 127
+}
+_start() {
+  if is_exists $app;then
+    sudo docker start $app
+  else
+    run
+  fi
+}
+###############
+case "$1" in
+  start)
+    _start
+    ;;
+  stop)
+    sudo docker stop -t 300 $app
+    ;;
+  debug)
+    run debug /bin/bash -l
+    ;;
+  rebuild)
+    sudo docker stop -t 300 $app
+    sudo docker rm $app
+    _start
+    ;;
+  *)
+    usage
+    ;;
+esac
